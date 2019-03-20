2fbdbd909de5116ad454689e5f6a1cb48c2a3ae0
Macarie Cristian <33750050+macarieCristian@users.noreply.github.com>
2019-03-20 13:21:57 +0200

    added some tests

---
+++ src/test/java/Service/RepositoryStudentTest.java
@@ -0,0 +1,35 @@
+package Service;
+
+import Domain.Student;
+import Repository.StudentRepo;
+import Validator.StudentValidator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
+
+public class RepositoryStudentTest {
+    private static final StudentRepo studentRepo = new StudentRepo(new StudentValidator(),"src/main/java/studenti.xml");
+
+    @Before
+    public void setUp() throws Exception {
+        studentRepo.save(new Student("11","name",111, "email@gmail.com", "prof"));
+    }
+
+    @Test
+    public void addStudentTest() {
+        assert StreamSupport.stream(studentRepo.findAll().spliterator(), false).count() == 3;
+        studentRepo.save(new Student("13","name",111, "email@gmail.com", "prof"));
+        assert StreamSupport.stream(studentRepo.findAll().spliterator(), false).count() == 4;
+    }
+
+    @Test
+    public void findStudentTest() {
+        assert studentRepo.findOne("11").getNume().equals("name");
+    }
+}
